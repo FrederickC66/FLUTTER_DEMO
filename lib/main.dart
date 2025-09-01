c7b86bce6516050393db115574d3442cba4ab3ef
Frederick Cortez <frederickcortez66@gmail.com>
2025-09-01 13:29:02 -0500

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ switch (selectedIndex) {
   case 0:
     page = GeneratorPage();
   case 1:
-    page = Placeholder();
+    page = FavoritesPage();
   default:
     throw UnimplementedError('no widget for $selectedIndex');
 }
@@ -95,6 +95,33 @@ switch (selectedIndex) {
           ),
         );
       }
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text('You have ${appState.favorites.length} ${appState.favorites.length == 1 ? 'favorite.' : 'favorites.'}'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
     );
   }
 }
